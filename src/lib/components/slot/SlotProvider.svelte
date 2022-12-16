0c81a6bccd420d05e433fe1fd50f8ba1b2e9ce45
Justinas Delinda <8914032+minht11@users.noreply.github.com>
2022-12-16 16:51:19 +0200

    Allow creating slot provider context directly

--- src/lib/components/slot/SlotProvider.svelte
+++ src/lib/components/slot/SlotProvider.svelte
@@ -1,18 +1,22 @@
 <script context="module" lang="ts">
+	import { setContext, getContext } from 'svelte'
+	import { writable, type Writable } from 'svelte/store'
+
 	const slotProviderKey = Symbol()
 
 	type Slots = Record<PropertyKey, HTMLElement>
+
+	export const createSlotProvider = () => {
+		const store = writable<Slots>({})
+
+		setContext(slotProviderKey, store)
+	}
 
 	export const getSlotContext = () => getContext<Writable<Slots>>(slotProviderKey)
 </script>
 
 <script lang="ts">
-	import { setContext, getContext } from 'svelte'
-	import { writable, type Writable } from 'svelte/store'
-
-	const store = writable<Slots>({})
-
-	setContext(slotProviderKey, store)
+	createSlotProvider()
 </script>
 
 <slot />
