819febc5f4ccf0f4b5abdfc1c78abe27b62fdd43
Justinas Delinda <8914032+minht11@users.noreply.github.com>
2022-12-16 03:02:22 +0200

    Add Icon and IconButton components

---
+++ src/lib/components/IconButton.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { clx } from '$lib/helpers/clx'
+	import Icon, { type IconType } from './icon/Icon.svelte'
+
+	export let icon: IconType
+	export let as: 'button' | 'a' = 'button'
+</script>
+
+<svelte:element
+	this={as}
+	{...$$restProps}
+	class={clx('flex items-center rounded-full h-44 w-44', $$props.class)}
+>
+	<Icon class="mx-auto" type={icon} />
+</svelte:element>
---
+++ src/lib/components/icon/Icon.svelte
@@ -0,0 +1,19 @@
+<script lang="ts" context="module">
+	import { clx } from '$lib/helpers/clx'
+	import { ICON_PATHS } from './icon-paths'
+
+	export type IconType = keyof typeof ICON_PATHS
+</script>
+
+<script lang="ts">
+	export let type: IconType
+</script>
+
+<svg
+	width="24"
+	height="24"
+	viewBox="0 0 24 24"
+	class={clx('fill-current pointer-events-none', $$props.class)}
+>
+	<path d={ICON_PATHS[type]} />
+</svg>
