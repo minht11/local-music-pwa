28681f055cf9456dc35a5dba4d0ac3c5b355e7fa
Justinas Delinda <8914032+minht11@users.noreply.github.com>
2023-09-03 03:00:16 +0300

    Add unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { sveltekit } from '@sveltejs/kit/vite'
+import UnoCSS from '@unocss/svelte-scoped/vite'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [UnoCSS(), sveltekit()],
 })
