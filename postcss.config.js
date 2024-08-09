7542c6589cf99a3d47343bee1fcf6e9a9cb0114b
Justinas Delinda <8914032+minht11@users.noreply.github.com>
2024-08-10 01:15:27 +0300

    remove autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 import unocss from '@unocss/postcss'
-import autoprefixer from 'autoprefixer'
 // import nesting from 'postcss-nesting'
 
 export default {
-	plugins: [unocss(), autoprefixer()],
+	plugins: [unocss()],
 }
