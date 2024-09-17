8af006b4e288da241d43be981d676f1196705d92
Justinas Delinda <8914032+minht11@users.noreply.github.com>
2024-09-17 22:08:35 +0300

    add csp

--- svelte.config.js
+++ svelte.config.js
@@ -18,8 +18,20 @@ const config = {
 		alias: {
 			$paraglide: './.generated/paraglide',
 		},
-		// TODO. Add csp options
-		// csp
+		csp: {
+			directives: {
+				'default-src': ['none'],
+				'script-src': ['self', 'https://gc.zgo.at/'],
+				'style-src': ['self'],
+				'img-src': ['self', 'blob:', 'https://snaeplayer.goatcounter.com/count'],
+				'media-src': ['self', 'blob:'],
+				'font-src': ['self'],
+				'connect-src': ['self', 'https://snaeplayer.goatcounter.com'],
+				'form-action': ['none'],
+				'manifest-src': ['self'],
+				'base-uri': ['none'],
+			},
+		},
 	},
 }
 
