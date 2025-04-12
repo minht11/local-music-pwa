20eef1404bf6e84de8510f8598da11d91a0a7b50
Justinas Delinda <8914032+minht11@users.noreply.github.com>
2025-04-12 23:49:09 +0300

    remove eager option

--- src/lib/db/query/query.ts
+++ src/lib/db/query/query.ts
@@ -1,26 +1,19 @@
 import {
-	type QueryBaseOptions,
 	QueryImpl,
 	type QueryKey,
+	type QueryBaseOptions as QueryOptions,
 	type QueryResult,
 	QueryResultBox,
 } from './base-query.svelte.ts'
 
 export type { QueryKey, QueryResult } from './base-query.svelte.ts'
 
-export interface QueryOptions<K extends QueryKey, Result>  extends QueryBaseOptions<K, Result> {
-	/**
-	 * Whether to load the query immediately
-	 * @default false
-	 */
-	eager?: boolean
+export type {
+	QueryOptions,
 }
 
 export const createQuery = <const K extends QueryKey, Result>(options: QueryOptions<K, Result>) => {
 	const query = new QueryImpl<K, Result>(options)
-	if (options.eager) {
-		void query.load()
-	}
 	query.setupListeners()
 
 	return new QueryResultBox(query.state) as QueryResult<Result>
