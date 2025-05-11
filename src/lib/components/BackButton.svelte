5462d1d59fd9734af825e36c9db5f1dfc117eccb
Justinas Delinda <8914032+minht11@users.noreply.github.com>
2025-05-11 14:23:18 +0300

    fix back button

--- src/lib/components/BackButton.svelte
+++ src/lib/components/BackButton.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import { goto } from '$app/navigation'
-	import { page } from '$app/state'
-	import { wait } from '$lib/helpers/utils/wait.ts'
 	import IconButton from './IconButton.svelte'
 
 	interface Props {
@@ -10,35 +8,21 @@
 
 	const { class: className }: Props = $props()
 
-	const goHome = () => {
-		void goto('/library/tracks')
+	const canGoBack = () => {
+		if (window.navigation !== undefined) {
+			return window.navigation.canGoBack
+		}
+
+		// This will not handle
+		return window.history.length > 1
 	}
 
 	const handleBackClick = () => {
-		if (window.navigation !== undefined) {
-			if (window.navigation.canGoBack) {
-				window.navigation.back()
-			} else {
-				goHome()
-			}
-
-			return
+		if (canGoBack()) {
+			window.history.back()
+		} else {
+			void goto('/library/tracks')
 		}
-
-		const path = page.url.pathname
-		window.history.back()
-		// If there are no entries inside history back button
-		// won't work and user will be stuck.
-		// Example: user loads new tab going straight to /settings,
-		// when app back button is pressed, nothing happens because
-		// this is the first entry in history.
-		// To prevent this check if URL changed, after short delay,
-		// if it didn't back button most likely didn't do anything.
-		void wait(50).then(() => {
-			if (path === page.url.pathname) {
-				goHome()
-			}
-		})
 	}
 </script>
 
