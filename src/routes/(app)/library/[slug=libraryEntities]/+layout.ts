35c7fd8504746d7118e74305f6c4f9bc8b924dfc
Justinas Delinda <8914032+minht11@users.noreply.github.com>
2025-05-21 00:19:50 +0900

    update deps

--- src/routes/(app)/library/[slug=libraryEntities]/+layout.ts
+++ src/routes/(app)/library/[slug=libraryEntities]/+layout.ts
@@ -28,7 +28,7 @@ type LoadDataResult<Slug extends LibraryStoreName> = {
 	}
 }[Slug]
 
-const loadData = async <Slug extends LibraryStoreName>(slug: Slug): Promise<LoadDataResult<Slug>> => {
+const loadData = <Slug extends LibraryStoreName>(slug: Slug): Promise<LoadDataResult<Slug>> => {
 	const config = configsMap[slug]
 	const searchFn = config.search ?? defaultSearchFn
 	const store = new LibraryStore(slug)
@@ -51,17 +51,24 @@ const loadData = async <Slug extends LibraryStoreName>(slug: Slug): Promise<Load
 		},
 	})
 
-	const [itemsIdsQuery, tracksCountQuery] = await Promise.all([
-		await itemsIdsQueryPromise,
-		createTracksCountPageQuery(),
-	])
-
-	return {
-		...config,
-		store,
-		itemsIdsQuery,
-		tracksCountQuery,
+	// This extra function wrapper is needed because of
+	// https://github.com/sveltejs/kit/issues/13809
+	// TODO. Remove this when the issue is fixed
+	const getAsync = async () => {
+		const [itemsIdsQuery, tracksCountQuery] = await Promise.all([
+			await itemsIdsQueryPromise,
+			createTracksCountPageQuery(),
+		])
+	
+		return {
+			...config,
+			store,
+			itemsIdsQuery,
+			tracksCountQuery,
+		}
 	}
+
+	return getAsync()
 }
 
 type LoadResult = LoadDataResult<LibraryStoreName> & {
