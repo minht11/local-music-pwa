426a06842fc5d6adf6ded23db2fc15732e84f6a8
Justinas Delinda <8914032+minht11@users.noreply.github.com>
2025-06-15 00:45:48 +0300

    use css file as source for server theme palette

--- src/server/theme-colors.ts
+++ src/server/theme-colors.ts
@@ -1,18 +1,24 @@
-import { getThemePaletteRgbEntries, type PaletteToken } from '$lib/theme.ts'
-// biome-ignore lint/style/noRestrictedImports: used in server
-import { argbFromHex } from '@material/material-color-utilities'
-
-// TODO. Ideally our Vite theme plugin would generate .ts file with these colors
-// so we don't need to have two places where colors are defined.
-const defaultColorSeed = '#cc9724'
-const argb = argbFromHex(defaultColorSeed)
+import type { PaletteToken } from '$lib/theme.ts'
+import themeCss from '../../.generated/theme-colors.css?raw'
 
 /** @public */
-export const THEME_PALLETTE_LIGHT = Object.fromEntries(
-	getThemePaletteRgbEntries(argb, false),
-) as Record<PaletteToken, string>
+export const THEME_PALLETTE_LIGHT = {} as Record<PaletteToken, string>
 
 /** @public */
-export const THEME_PALLETTE_DARK = Object.fromEntries(
-	getThemePaletteRgbEntries(argb, true),
-) as Record<PaletteToken, string>
+export const THEME_PALLETTE_DARK = {} as Record<PaletteToken, string>
+
+const regex =
+	/--color-([a-zA-Z0-9-]+):\s*light-dark\(\s*(#[a-fA-F0-9]{3,6})\s*,\s*(#[a-fA-F0-9]{3,6})\s*\)/g
+
+let match: RegExpExecArray | null
+// biome-ignore lint/suspicious/noAssignInExpressions: extracting colors from CSS
+while ((match = regex.exec(themeCss)) !== null) {
+	const colorName = match[1] as PaletteToken
+	const lightColor = match[2]
+	const darkColor = match[3]
+
+	invariant(lightColor && darkColor, `Invalid color definition for ${colorName}`)
+
+	THEME_PALLETTE_LIGHT[colorName] = lightColor
+	THEME_PALLETTE_DARK[colorName] = darkColor
+}
