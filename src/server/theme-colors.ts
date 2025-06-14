b0d4d9ab519c584636214168b01826ee5f5be89d
Justinas Delinda <8914032+minht11@users.noreply.github.com>
2025-06-14 16:32:30 +0300

    add comment

--- src/server/theme-colors.ts
+++ src/server/theme-colors.ts
@@ -2,6 +2,8 @@ import { getThemePaletteRgbEntries, type PaletteToken } from '$lib/theme.ts'
 // biome-ignore lint/style/noRestrictedImports: used in server
 import { argbFromHex } from '@material/material-color-utilities'
 
+// TODO. Ideally our Vite theme plugin would generate .ts file with these colors
+// so we don't need to have two places where colors are defined.
 const defaultColorSeed = '#cc9724'
 const argb = argbFromHex(defaultColorSeed)
 
