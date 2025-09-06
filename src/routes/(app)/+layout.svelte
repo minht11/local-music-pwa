b230653889acc3924bdb49f9f9b50132e0c2f61d
Justinas Delinda <8914032+minht11@users.noreply.github.com>
2025-09-07 01:41:32 +0300

    format

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import { setContext } from 'svelte'
+	import { browser } from '$app/environment'
 	import { navigating, page } from '$app/state'
 	import { APP_DIALOGS_COMPONENTS } from '$lib/components/app-dialogs/dialogs.ts'
+	import Button from '$lib/components/Button.svelte'
+	import Icon from '$lib/components/icon/Icon.svelte'
 	import MenuRenderer, { setupGlobalMenu } from '$lib/components/menu/MenuRenderer.svelte'
 	import PlayerOverlay from '$lib/components/PlayerOverlay.svelte'
 	import SnackbarRenderer from '$lib/components/snackbar/SnackbarRenderer.svelte'
@@ -12,14 +15,11 @@
 	import { PLAYER_STORE_CONTEXT } from '$lib/stores/player/use-store.ts'
 	import { onViewTransitionPrepare, setupAppViewTransitions } from '$lib/view-transitions.svelte.ts'
 	import { setupAppInstallPromptListeners } from './layout/app-install-prompt.ts'
-	import { setupTheme } from './layout/setup-theme.svelte.ts'
-	import { browser } from '$app/environment'
 	import {
-		setupDirectoriesPermissionPrompt,
 		type DirectoriesPermissionPromptSnackbarArg,
+		setupDirectoriesPermissionPrompt,
 	} from './layout/setup-directories-permission-prompt.svelte.ts'
-	import Button from '$lib/components/Button.svelte'
-	import Icon from '$lib/components/icon/Icon.svelte'
+	import { setupTheme } from './layout/setup-theme.svelte.ts'
 
 	// These context keys are in different files from their implementation
 	// to allow better trees shaking and inlining
