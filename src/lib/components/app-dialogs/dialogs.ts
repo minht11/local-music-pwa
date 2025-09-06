d6b912fa668a96c53ec0e306475bf2826ecce673
Justinas Delinda <8914032+minht11@users.noreply.github.com>
2025-09-07 01:42:26 +0300

    fix biome

--- src/lib/components/app-dialogs/dialogs.ts
+++ src/lib/components/app-dialogs/dialogs.ts
@@ -1,7 +1,9 @@
-// biome-ignore-all lint/suspicious/noExplicitAny: false positive
 import ConfirmRemoveLibraryItem from './ConfirmRemoveLibraryItem.svelte'
+// biome-ignore lint/correctness/noPrivateImports: false positive
 import AddToPlaylistDialog from './playlists/AddToPlaylistDialog.svelte'
+// biome-ignore lint/correctness/noPrivateImports: false positive
 import EditPlaylistDialog from './playlists/EditPlaylistDialog.svelte'
+// biome-ignore lint/correctness/noPrivateImports: false positive
 import NewPlaylistDialog from './playlists/NewPlaylistDialog.svelte'
 
 export const APP_DIALOGS_COMPONENTS = [
