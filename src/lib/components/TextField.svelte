32eaca19a6f2b684e8f6fe292ac647e008f58c9b
Justinas Delinda <8914032+minht11@users.noreply.github.com>
2026-01-17 21:03:01 +0200

    remove valibot

--- src/lib/components/TextField.svelte
+++ src/lib/components/TextField.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import * as v from 'valibot'
-
 	interface TextFieldProps {
 		value?: string
 		name: string
@@ -25,31 +23,21 @@
 
 	const id = crypto.randomUUID()
 
-	const ValidationsSchema = $derived.by(() => {
-		const validators: unknown[] = [v.string()]
-
-		if (required) {
-			validators.push(v.minLength(1, m.validationRequired()))
+	const validationIssue = $derived.by(() => {
+		const valueLength = value.length
+		if (required && valueLength < 1) {
+			return m.validationRequired()
 		}
 
-		if (minLength !== undefined) {
-			validators.push(v.minLength(minLength, m.validationMinLength({ min: minLength })))
+		if (minLength !== undefined && valueLength < minLength) {
+			return m.validationMinLength({ min: minLength })
 		}
 
-		if (maxLength !== undefined) {
-			validators.push(v.maxLength(maxLength, m.validationMaxLength({ max: maxLength })))
+		if (maxLength !== undefined && valueLength > maxLength) {
+			return m.validationMaxLength({ max: maxLength })
 		}
 
-		// @ts-expect-error not sure how to type this
-		return v.pipe(...validators)
-	})
-
-	const validation = $derived(v.safeParse(ValidationsSchema, value))
-
-	let input = $state<HTMLInputElement | null>(null)
-
-	$effect(() => {
-		input?.setCustomValidity(validation.success ? '' : ' ')
+		return null
 	})
 </script>
 
@@ -58,7 +46,6 @@
 		class="flex h-14 flex-col rounded-md border border-outline p-px text-onSurface focus-within:border-2 focus-within:border-primary focus-within:p-0 [&:has(input:user-invalid)]:border-error"
 	>
 		<input
-			bind:this={input}
 			bind:value
 			{name}
 			{id}
@@ -66,10 +53,13 @@
 			{required}
 			class="w-full grow appearance-none border-none bg-transparent px-3.5 outline-none placeholder:text-onSurfaceVariant"
 			{placeholder}
+			{@attach (input) => {
+				input.setCustomValidity(validationIssue ? ' ' : '')
+			}}
 		/>
 	</div>
 	<div class="text-field-error mt-1 hidden px-4 text-body-sm text-error">
-		{validation.issues?.at(0)?.message ?? ''}
+		{validationIssue ?? ''}
 	</div>
 </div>
 
