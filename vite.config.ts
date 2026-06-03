2c1a6dc6f906da30eb7922a8203a34db485b59c0
Justinas Delinda <8914032+minht11@users.noreply.github.com>
2026-06-03 22:47:43 +0300

    types and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { paraglideVitePlugin } from '@inlang/paraglide-js'
 import { sveltekit } from '@sveltejs/kit/vite'
 import tailwindcss from '@tailwindcss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
+import { i18nPlugin } from './lib/vite-i18n/plugin.ts'
 import { imageMetadataPlugin } from './lib/vite-image-metadata.ts'
 import { logChunkSizePlugin } from './lib/vite-log-chunk-size.ts'
 
@@ -11,7 +11,7 @@ const getAutoImportPlugin = (dts: string | false = false) =>
 		dts,
 		imports: [
 			{
-				'$paraglide/messages': [['*', 'm']],
+				'i18n:messages': [['*', 'm']],
 				'$lib/stores/player/use-store.ts': ['usePlayer'],
 				'$lib/stores/main/use-store.ts': ['useMainStore'],
 				'$lib/stores/dialogs/use-store.ts': ['useDialogsStore'],
@@ -70,11 +70,11 @@ export default defineConfig({
 		imageMetadataPlugin(),
 		tailwindcss(),
 		sveltekit(),
-		paraglideVitePlugin({
-			project: './project.inlang',
-			outdir: './.generated/paraglide',
-			strategy: ['baseLocale'],
-			isServer: 'import.meta.env.SSR',
+		i18nPlugin({
+			inputDir: 'messages',
+			outputDir: './.generated/i18n',
+			baseLocale: 'en',
+			locales: ['en', 'lt', 'de', 'fr', 'zh-CN', 'zh-TW'],
 		}),
 		getAutoImportPlugin('./.generated/types/auto-imports.d.ts'),
 		logChunkSizePlugin(),
